f0c328bfd9783207e2c9aa88da8379df5f66d6f7
utgwkk <utagawakiki@gmail.com>
2020-06-19 17:10:53 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Test2::Plugin::GitHub::Actions::AnnotateWarnings - It's new $module
+
+# SYNOPSIS
+
+    use Test2::Plugin::GitHub::Actions::AnnotateWarnings;
+
+# DESCRIPTION
+
+Test2::Plugin::GitHub::Actions::AnnotateWarnings is ...
+
+# LICENSE
+
+Copyright (C) utgwkk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+utgwkk <utagawakiki@gmail.com>
---
+++ lib/Test2/Plugin/GitHub/Actions/AnnotateWarnings.pm
@@ -0,0 +1,39 @@
+package Test2::Plugin::GitHub::Actions::AnnotateWarnings;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test2::Plugin::GitHub::Actions::AnnotateWarnings - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test2::Plugin::GitHub::Actions::AnnotateWarnings;
+
+=head1 DESCRIPTION
+
+Test2::Plugin::GitHub::Actions::AnnotateWarnings is ...
+
+=head1 LICENSE
+
+Copyright (C) utgwkk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+utgwkk E<lt>utagawakiki@gmail.comE<gt>
+
+=cut
+
